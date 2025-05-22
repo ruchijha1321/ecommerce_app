6a21b8a09f2d835503bd5630e912cfb89bfb307b
ruchijha1321 <ruchikr161@gmail.com>
2025-05-22 14:28:49 +0530

    Update Header.js

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -21,7 +21,7 @@ export default function Header () {
         <div className="col-12 col-md-3">
           <div className="navbar-brand">
             <Link to="/">
-              <img width="150px" alt='JVLcart Logo' src="/images/logo.png" />
+              <img width="150px" alt='SmileShop' src="/images/logo.png" />
             </Link>
             </div>
         </div>
